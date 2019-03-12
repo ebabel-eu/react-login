a1ec3302a7ee9ff18fb15d310754e45a3b8354f9
Thomas Amar <hello@ebabel.eu>
2019-03-12 01:46:08 +0100

    Fix demo libraryTarget from commonjs2 to umd for browser demo.

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = [
     output: {
       path: path.resolve(__dirname, './'),
       filename: 'demo/demo.js',
-      libraryTarget: 'commonjs2' 
+      libraryTarget: 'umd' 
     },
     module: {
       rules: [
