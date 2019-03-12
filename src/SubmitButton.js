8c097ad63b89d8347728878297f5f04359abd031
Thomas Amar <hello@ebabel.eu>
2019-03-12 10:25:19 +0100

    Fix setting the value of the SubmitButton component.

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SubmitButton = ({name, label, disabled}) => (
- <input type="submit" name={name} defaultValue={label} disabled={disabled} />
+ <input type="submit" name={name} value={`${label}`} disabled={disabled} />
 );
 
 SubmitButton.defaultProps = {
-  label: 'Send',
   disabled: true,
 };
 
 SubmitButton.propTypes = {
   name: PropTypes.string.isRequired,
-  label: PropTypes.string,
+  label: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
 };
 
