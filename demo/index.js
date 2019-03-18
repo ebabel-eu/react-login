b03025074a180e3a9c92172bac9d07c165e2efd0
Thomas Amar <hello@ebabel.eu>
2019-03-18 20:09:33 +0100

    Add redux-devtools-extension dependency to demo.

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './app';
 import reducers from './reducers';
 
 const root = document.getElementById('root');
-const store = createStore(reducers);
+const store = createStore(reducers, /* preloadedState, */
+  composeWithDevTools(
+    // applyMiddleware(...middleware), Note: applyMiddleware can be imported from 'redux'.
+    // other store enhancers if any
+  ));
 
 ReactDOM.render(
   <Provider store={store}>
