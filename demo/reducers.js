a64deb552cb318120b796e305d344067d9fdf263
Thomas Amar <hello@ebabel.eu>
2019-03-18 17:40:05 +0000

    Add react-redux and redux dependencies for the demo.

---
+++ demo/reducers.js
@@ -0,0 +1,36 @@
+const initialState = {};
+const SET_VISIBILITY_FILTER = 'set-visibility-filter';
+const ADD_TODO = 'add-todo';
+const TOGGLE_TODO = 'toggle-todo';
+
+module.exports = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        })
+      })
+    default:
+      return state
+  }
+};
